74e0dbe578e5564477cc54842e668b274b5077dc
Tusharkumar-dev <githubtk1007@gmail.com>
2025-12-26 23:34:52 +0530

    added genpass logic

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 #!/usr/bin/env node
 
 import clipboard from 'clipboardy';
-import {genLowerLetters,
-	genUpperLetters,
-	genNumbers,
-	genSymbols,
-	shuffleMax,
-	genPassword,
-	randomNumIn,
-	shuffle,
+import {
+  genLowerLetters,
+  genUpperLetters,
+  genNumbers,
+  genSymbols,
+  shuffleMax,
+  genPassword,
+  randomNumIn,
+  shuffle,
 } from './utility.js';
 
 const flag = process.argv[2]
+const isCalledByGenPass = process.argv[1].includes("@tushardev01");
 
-if(flag && flag==='-c'){
-  const password=genPassword();
+if (isCalledByGenPass) {
+  const password = genPassword();
   console.log(password);
-  
-  clipboard.writeSync(password)
-  console.log("Copied to clipboard!!");
+
+  if (flag && flag === '-c') {
+    clipboard.writeSync(password)
+    console.log("Copied to clipboard!!");
+  }
 }
-export{
+export {
   genLowerLetters,
   genUpperLetters,
   genNumbers,
