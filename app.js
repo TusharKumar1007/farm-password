c2822d66b373b2663d27b13e89cf491b40f6ba99
Tusharkumar-dev <githubtk1007@gmail.com>
2025-12-26 23:19:45 +0530

    added -c flag to copy password

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import clipboard from 'clipboardy';
 import {genLowerLetters,
 	genUpperLetters,
 	genNumbers,
@@ -10,7 +11,15 @@ import {genLowerLetters,
 	shuffle,
 } from './utility.js';
 
+const flag = process.argv[2]
 
+if(flag && flag==='-c'){
+  const password=genPassword();
+  console.log(password);
+  
+  clipboard.writeSync(password)
+  console.log("Copied to clipboard!!");
+}
 export{
   genLowerLetters,
   genUpperLetters,
